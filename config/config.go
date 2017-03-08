f3e5b2d193bed071a92711f350b76fe47e4a87b9
Kumar Mantesh <kumar.mantesh007@gmail.com>
2017-03-08 08:16:08 +0530

    Changed algo for configs
    Automatically gets flag params from config file, path to external config
    is provided by environment variable.

--- config/config.go
+++ config/config.go
@@ -5,96 +5,81 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 
 	"gopkg.in/yaml.v2"
 )
 
 var y map[string]interface{}
-var args map[string]interface{}
 
 //initialize the configs by reading them from the config files
 func init() {
-	initCmdArgs()
 	initYaml()
-
-}
-
-func initCmdArgs() {
-	args = map[string]interface{}{
-		"pg_uname":    nil,
-		"pg_pass":     nil,
-		"pg_name":     nil,
-		"pg_ip":       nil,
-		"pg_port":     nil,
-		"redis_uname": nil,
-		"redis_pass":  nil,
-		"redis_port":  nil,
-		"redis_ip":    nil,
-	}
-	for key, _ := range args {
-		args[key] = flag.String(key, "", "")
-	}
-	flag.Parse()
+	initCmdArgs()
 }
 
 func initYaml() {
-	var s = string(os.PathSeparator)
-	config, err := ioutil.ReadFile(s + "etc" + s + "galore" + s + "config.yaml")
+	configFilePath := os.Getenv("GALORE_CONFIG")
+	// Use local config file
+	if len(configFilePath) == 0 {
+		seperator := string(os.PathSeparator)
+		configFilePath = "config" + seperator + "config.yaml"
+	}
+	config, err := ioutil.ReadFile(configFilePath)
 	if err != nil {
-		log.Println("Warning: config file /etc/galore/config.yaml not found")
-	} else {
-		y = make(map[string]interface{})
-		err = yaml.Unmarshal([]byte(config), &y)
-		if err != nil {
-			log.Fatalf("Error reading yaml file: %v", err)
+		log.Println("Warning: config file not found")
+		return
+	}
+	y = make(map[string]interface{})
+	err = yaml.Unmarshal([]byte(config), &y)
+	if err != nil {
+		log.Fatalf("Error reading yaml file: %v", err)
+	}
+}
+
+func initCmdArgs() {
+	t := make(map[string]interface{})
+	for key, _ := range y {
+		if _, ok := y[key].(string); ok {
+			t[key] = flag.String(key, "", "")
+		}
+		if _, ok := y[key].(int); ok {
+			t[key] = flag.Int(key, 0, "")
+		}
+		if _, ok := y[key].(bool); ok {
+			t[key] = flag.Bool(key, false, "")
+		}
+	}
+	flag.Parse()
+	for key, _ := range y {
+		if _, ok := y[key].(string); ok {
+			if x, ok := t[key].(*string); ok && len(*x) > 0 {
+				y[key] = *x
+			}
+		}
+		if _, ok := y[key].(int); ok {
+			if x, ok := t[key].(*int); ok && *x > 0 {
+				y[key] = *x
+			}
+		}
+		if _, ok := y[key].(bool); ok {
+			if x, ok := t[key].(*bool); ok && *x != false {
+				y[key] = *x
+			}
 		}
 	}
 }
 
 func GetString(key string) (val string, ok bool) {
-	valPtr, ok := args[key].(*string)
-	if ok && len(*valPtr) != 0 {
-		val = *valPtr
-	}
 	val, ok = y[key].(string)
-	if ok {
-		return
-	}
 	return
 }
 
 func GetInt(key string) (val int, ok bool) {
-	var err error
-	valPtr, ok := args[key].(*string)
-	if ok && len(*valPtr) != 0 {
-		val, err = strconv.Atoi(*valPtr)
-		if err != nil {
-			panic(key + " is not of type int")
-		}
-	}
 	val, ok = y[key].(int)
-	if ok {
-		return
-	}
 	return
 }
 
 func GetBool(key string) (val bool, ok bool) {
-	valPtr, ok := args[key].(*string)
-	if ok && len(*valPtr) != 0 {
-		if *valPtr == strings.ToUpper("true") {
-			val = true
-		} else if *valPtr == strings.ToUpper("false") {
-			val = false
-		} else {
-			panic(key + " is not of type bool")
-		}
-	}
 	val, ok = y[key].(bool)
-	if ok {
-		return
-	}
 	return
 }
